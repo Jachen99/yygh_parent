05cbd18fb702741b955072a22e824a797e2f428c
Jachen <914158173@qq.com>
2023-01-28 18:24:54 +0800

    登录功能1.0

---
+++ service/service_hosp/src/main/java/space/jachen/yygh/hosp/controller/LoginController.java
@@ -0,0 +1,40 @@
+package space.jachen.yygh.hosp.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+import space.jachen.yygh.common.result.JsonData;
+
+import java.util.HashMap;
+
+/**
+ * @author JaChen
+ * @date 2023/1/28 16:40
+ */
+@Api(tags = "登录接口")
+@RestController
+@RequestMapping("/admin/hosp/user")
+@CrossOrigin
+public class LoginController {
+
+    @ApiOperation("登录")
+    @PostMapping("/login")
+    public JsonData<HashMap<String, String>> login(){
+        //{"code":20000,"data":{"token":"admin-token"}}
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("token","admin-token");
+        return JsonData.ok(hashMap);
+    }
+
+    @ApiOperation("获取用户信息")
+    @GetMapping("/info")
+    public JsonData<HashMap<String, String>> info(){
+        //{"code":20000,"data":{"roles":["admin"],"introduction":"I am a super administrator","avatar":"https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif","name":"Super Admin"}}
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("name","欢迎季老板！");
+        hashMap.put("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        return JsonData.ok(hashMap);
+    }
+
+}

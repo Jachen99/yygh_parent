5b72e932624a8875823c6655fdf129fa21a34d91
Jachen <914158173@qq.com>
2023-02-13 11:54:06 +0800

    增加微信支付配置
    Signed-off-by: Jachen <914158173@qq.com>

---
+++ service/service_orders/src/main/java/space/jachen/yygh/order/utils/ConstantPropertiesUtils.java
@@ -0,0 +1,35 @@
+package space.jachen.yygh.order.utils;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConstantPropertiesUtils implements InitializingBean {
+
+    @Value("${wx.pay.appid}")
+    private String appid;
+
+    @Value("${wx.pay.partner}")
+    private String partner;
+
+    @Value("${wx.pay.partner-key}")
+    private String partnerkey;
+
+    @Value("wx.cert")
+    private String cert;
+
+    public static String APPID;
+    public static String PARTNER;
+    public static String PARTNERKEY;
+    public static String CERT;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        APPID = appid;
+        PARTNER = partner;
+        PARTNERKEY = partnerkey;
+        CERT = cert;
+    }
+}
+

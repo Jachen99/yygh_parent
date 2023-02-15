f1d61e27ac77b8fd631a8ed1b30f3c376f32b3f4
Jachen <914158173@qq.com>
2023-02-15 10:15:59 +0800

    新增视图模块统计管理接口✌
    Signed-off-by: Jachen <914158173@qq.com>

---
+++ service/service_statistics/src/main/java/space/jachen/yygh/statistics/controller/StatisticsController.java
@@ -0,0 +1,31 @@
+package space.jachen.yygh.statistics.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import space.jachen.yygh.common.result.JsonData;
+import space.jachen.yygh.statistics.OrderFeignClient;
+import space.jachen.yygh.vo.order.OrderCountQueryVo;
+
+import java.util.Map;
+
+/**
+ * @author JaChen
+ * @date 2023/2/15 10:14
+ */
+@Api(tags = "统计管理接口")
+@RestController
+@RequestMapping("/admin/statistics")
+public class StatisticsController {
+    @Autowired
+    private OrderFeignClient orderFeignClient;
+    @ApiOperation(value = "获取订单统计数据")
+    @GetMapping("getCountMap")
+    public JsonData<Map<String, Object>> getCountMap(OrderCountQueryVo orderCountQueryVo) {
+        Map<String, Object> map = orderFeignClient.getCountMap(orderCountQueryVo);
+        return JsonData.ok(map);
+    }
+}
